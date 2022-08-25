ad57cf036a72e4cc41efa95c625b8521db516ee8
Stefan Froemken <froemken@gmail.com>
2022-08-25 13:49:06 +0200

    Set default value for files in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE tx_circular_domain_model_circular (
 	date_of_circular int(11) DEFAULT '0' NOT NULL,
 	send tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	department int(11) unsigned DEFAULT '0',
-	files mediumtext NOT NULL,
+	files varchar(255) DEFAULT '' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
