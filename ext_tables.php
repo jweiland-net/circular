1d948d5120f368a5cfeb0b8126ebc1f766bec64c
Stefan Froemken <froemken@gmail.com>
2023-03-02 14:35:52 +0100

    Remove BE module

--- ext_tables.php
+++ ext_tables.php
@@ -4,21 +4,6 @@ if (!defined('TYPO3_MODE')) {
 }
 
 call_user_func(static function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Circular',
-        'web',     // Make module a submodule of 'web'
-        'circular', // Submodule key
-        '',               // Position
-        [
-            \JWeiland\Circular\Controller\MaintenanceController::class => 'list, show, delete, prepare',
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:circular/Resources/Public/Icons/Extension.svg',
-            'labels' => 'LLL:EXT:circular/Resources/Private/Language/locallang_circular.xlf',
-        ]
-    );
-
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
         'tx_circular_domain_model_circular',
         'EXT:circular/Resources/Private/Language/locallang_csh_tx_circular_domain_model_circular.xlf'
