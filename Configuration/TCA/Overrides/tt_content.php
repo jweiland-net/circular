b03f8b190f7e72ec1720159874ad0b96f2f059f5
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-11 15:18:51 +0200

    [TASK] TCA header inclusion

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file is part of the package jweiland/circular.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
@@ -6,12 +14,12 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Circular',
     'Circular',
-    'LLL:EXT:circular/Resources/Private/Language/locallang_db.xlf:plugin.circular.title'
+    'LLL:EXT:circular/Resources/Private/Language/locallang_db.xlf:plugin.circular.title',
 );
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['circular_circular'] = 'pi_flexform';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     'circular_circular',
-    'FILE:EXT:circular/Configuration/FlexForms/Circular.xml'
+    'FILE:EXT:circular/Configuration/FlexForms/Circular.xml',
 );
