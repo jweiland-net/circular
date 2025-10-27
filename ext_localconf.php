21ca16335ab1622d3d7c406f1f8dfe68279f6de5
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-10-27 16:06:58 +0100

    [TASK] Fixed ext_localconf by removing old configurations

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,39 +11,20 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+use JWeiland\Circular\Controller\CircularController;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 call_user_func(static function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'Circular',
         'Circular',
         [
-            \JWeiland\Circular\Controller\CircularController::class => 'list, show',
+            CircularController::class => 'list, show',
         ],
         // non-cacheable actions
         [
-            \JWeiland\Circular\Controller\CircularController::class => '',
+            CircularController::class => '',
         ],
+        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
     );
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['circularFiles']
-        = \JWeiland\Circular\Updates\FilesUpdateWizard::class;
-
-    // Add circular plugin to new element wizard
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:circular/Configuration/TSconfig/ContentElementWizard.tsconfig">',
-    );
-
-    // Register SVG Icon Identifier
-    $svgIcons = [
-        'ext-circular-category' => 'category.svg',
-        'ext-circular-circular' => 'circular.svg',
-        'ext-circular-department' => 'department.svg',
-    ];
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($svgIcons as $identifier => $fileName) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => 'EXT:circular/Resources/Public/Icons/' . $fileName],
-        );
-    }
 });
